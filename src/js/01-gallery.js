f2d5230e0aca3e850c82f708d2a4945254b8ea56
nhetsko <nadiahecko@gmail.com>
2023-12-14 18:38:47 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Описаний в документації
 import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
 
+
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+// rendered items
+function createGalleryItemsMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+  <li class="gallery-item">
+    <a class="gallery-link" href=${original}>
+        <img 
+            class="gallery-image" 
+            src=${preview}
+            alt=${description}
+            />
+    </a>
+</li>
+`
+  }).join('');
+}
+
+// // use library SimpleLightbox
+// const lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+// });
