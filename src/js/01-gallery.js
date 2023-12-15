da558006e5078374388403a62452800bd57c3824
nhetsko <nadiahecko@gmail.com>
2023-12-15 18:23:21 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,31 +1,25 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-// Add imports above this line
+
+
 import { galleryItems } from './gallery-items';
 
 
-const galleryContainer = document.querySelector('.gallery');
-const itemsMarkup = createGalleryItemsMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+const gallery = document.querySelector('.gallery');
 
-// rendered items
-function createGalleryItemsMarkup(items) {
-  return items.map(({ preview, original, description }) => {
-    return `
-  <li class="gallery-item">
-    <a class="gallery-link" href=${original}>
-        <img 
-            class="gallery-image" 
-            src=${preview}
-            alt=${description}
-            />
-    </a>
-</li>
-`
-  }).join('');
-}
+const markup = galleryItems.map(({preview, original, description}) => `<li class="gallery-item">
+  <a class="gallery-link" href="${original}">
+    <img
+      class="gallery-image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('');
 
-// // use library SimpleLightbox
-// const lightbox = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
-// });
+gallery.innerHTML = markup;
+
+// use library SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
